384359ca94a52baf3989573c6a5a6ec76bb5e923
chewzzz <chewziqing@gmail.com>
2023-06-12 23:30:43 +0800

    edit quantity in stock

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,25 +5,25 @@ INSERT INTO products(category_id, price, quantity_in_stock, description, image, 
     VALUES(1, 79, 50, 'Perfect for Summer Days', 'guy1.png', 'Blue Striped Casual Dress');
 
 INSERT INTO products(category_id, price, quantity_in_stock, description, image, product_name)
-    VALUES(2, 129, 50, 'Elegant and Flowy', 'girl2.png', 'Pink Floral Maxi Dress');
+    VALUES(2, 129, 23, 'Elegant and Flowy', 'girl2.png', 'Pink Floral Maxi Dress');
 
 INSERT INTO products(category_id, price, quantity_in_stock, description, image, product_name)
-    VALUES(2, 89, 50, 'Classic and Chic', 'girl3.png', 'White Lace Blouse');
+    VALUES(2, 89, 30, 'Classic and Chic', 'girl3.png', 'White Lace Blouse');
 
 INSERT INTO products(category_id, price, quantity_in_stock, description, image, product_name)
-    VALUES(1, 109, 50, 'Stylish and Versatile', 'guy2.png', 'Black Denim Jeans');
+    VALUES(1, 109, 15, 'Stylish and Versatile', 'guy2.png', 'Black Denim Jeans');
 
 INSERT INTO products(category_id, price, quantity_in_stock, description, image, product_name)
     VALUES(1, 69, 50, 'Casual and Trendy', 'guy3.png', 'Red Checkered Shirt');
 
 INSERT INTO products(category_id, price, quantity_in_stock, description, image, product_name)
-VALUES(1, 49, 50, 'Fun and Flirty', 'guy5.png', 'Striped Crop Top');
+VALUES(1, 49, 30, 'Fun and Flirty', 'guy5.png', 'Striped Crop Top');
 
 INSERT INTO products(category_id, price, quantity_in_stock, description, image, product_name)
-VALUES(1, 99, 50, 'Playful and Vibrant', 'guy4.png', 'Yellow Ruffled Dress');
+VALUES(1, 99, 60, 'Playful and Vibrant', 'guy4.png', 'Yellow Ruffled Dress');
 
 INSERT INTO products(category_id, price, quantity_in_stock, description, image, product_name)
-VALUES(2, 79, 50, 'Chic and Sophisticated', 'girl4.png', 'Printed Midi Skirt');
+VALUES(2, 79, 100, 'Chic and Sophisticated', 'girl4.png', 'Printed Midi Skirt');
 
 INSERT INTO products(category_id, price, quantity_in_stock, description, image, product_name)
-VALUES(2, 89, 50, 'Quirky and Cute', 'ex2.png', 'Blue Polka Dot Dress');
+VALUES(2, 89, 70, 'Quirky and Cute', 'ex2.png', 'Blue Polka Dot Dress');
