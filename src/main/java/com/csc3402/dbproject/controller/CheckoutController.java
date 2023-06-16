238b10fe23eb3228b4cfe29b6737685ac9c32c55
chewzzz <chewziqing@gmail.com>
2023-06-16 22:05:07 +0800

    edit navbar link

--- src/main/java/com/csc3402/dbproject/controller/CheckoutController.java
+++ src/main/java/com/csc3402/dbproject/controller/CheckoutController.java
@@ -3,8 +3,10 @@ package com.csc3402.dbproject.controller;
 import com.csc3402.dbproject.model.CheckoutForm;
 import com.csc3402.dbproject.model.Order;
 import com.csc3402.dbproject.model.OrderProduct;
+import com.csc3402.dbproject.model.Product;
 import com.csc3402.dbproject.repository.OrderProductRepository;
 import com.csc3402.dbproject.repository.OrderRepository;
+import com.csc3402.dbproject.repository.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,10 +23,12 @@ import java.util.List;
 public class CheckoutController {
     private final OrderRepository orderRepository;
     private final OrderProductRepository orderProductRepository;
+    private final ProductRepository productRepository;
 
-    public CheckoutController(OrderRepository orderRepository, OrderProductRepository orderProductReposiotry) {
+    public CheckoutController(OrderRepository orderRepository, OrderProductRepository orderProductRepository, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
-        this.orderProductRepository = orderProductReposiotry;
+        this.orderProductRepository = orderProductRepository;
+        this.productRepository = productRepository;
     }
 
     @GetMapping("edit")
@@ -52,28 +56,40 @@ public class CheckoutController {
 
     @PostMapping("update")
     public RedirectView checkoutOrder(@RequestParam("order_id") long order_id, @ModelAttribute("checkout_form") CheckoutForm checkoutForm, BindingResult result, Model model, RedirectAttributes attributes) {
-//        if (result.hasErrors()) {
-//            System.out.println("There was a error "+result);
-//        }
-//        System.out.println(checkoutForm.toString());
-        System.out.println(checkoutForm.getOrder());
-        System.out.println(checkoutForm.getItemList());
+        try{
+            if (result.hasErrors()) {
+                System.out.println("There was a error "+result);
+            }
 
-        Order foundOrder = orderRepository.findById((int) order_id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid order Id:" + order_id));
-        Order order = checkoutForm.getOrder();
-        List<OrderProduct> cartList = checkoutForm.getItemList();
+            System.out.println(checkoutForm.getOrder());
+            System.out.println(checkoutForm.getItemList());
 
-        // update product quantity in stock
+            // get order details and list of items from wrapper received from form
+            Order foundOrder = orderRepository.findById((int) order_id)
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid order Id:" + order_id));
+            Order order = checkoutForm.getOrder();
+            List<OrderProduct> cartList = checkoutForm.getItemList();
 
-        // update order info
-//        foundOrder.setTotalprice(order.getTotalprice());
-//        foundOrder.setDate(new Timestamp(System.currentTimeMillis()));
-//        foundOrder.setPaymentmethod(order.getPaymentmethod());
-//        foundOrder.setAddress(order.getAddress());
-//        foundOrder.setPhonenum(order.getPhonenum());
-//        foundOrder.setHascheckout(1);
-//        orderRepository.save(foundOrder);
+            // update product quantity in stock
+            for(OrderProduct op : cartList) {
+                int product_id = op.getProduct().getProductId();
+                Product foundProduct = productRepository.findById(product_id)
+                        .orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + product_id));
+                foundProduct.setStockquantity(foundProduct.getStockquantity() - op.getQuantity());
+                productRepository.save(foundProduct);
+            }
+
+            // update order info
+            foundOrder.setTotalprice(order.getTotalprice());
+            foundOrder.setDate(new Timestamp(System.currentTimeMillis()));
+            foundOrder.setPaymentmethod(order.getPaymentmethod());
+            foundOrder.setAddress(order.getAddress());
+            foundOrder.setPhonenum(order.getPhonenum());
+            foundOrder.setHascheckout(1);
+            orderRepository.save(foundOrder);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
         attributes.addAttribute("order_id", (int) order_id);
         return new RedirectView("/orderlist/view") ;
